95988ce9cf9d9f28e91be7b75c72be3c2d541654
Alex Joedt <a.joedt@gmail.com>
2021-07-04 12:11:21 +0200

    add more urls

--- gomdirect_core.go
+++ gomdirect_core.go
@@ -12,9 +12,17 @@ const (
 	ActivateURL string = BaseURL + "api/session/clients/user/v1/sessions/%s"
 	OAuth2URL   string = BaseURL + "oauth/token"
 	RevokeURL   string = BaseURL + "oauth/revoke"
-)
 
-// Depot
-const (
-	DepotURL string = BaseURL + "/brokerage/clients/user/v3/depots"
+	// Depot
+	DepotURL             string = BaseURL + "/brokerage/clients/user/v3/depots"
+	DepotPortfolioURL    string = BaseURL + "/brokerage/v3/depots/%s/positions"
+	DepotPositionURL     string = BaseURL + "/brokerage/v3/depots/%s/positions/%s"
+	DepotTransactionsURL string = BaseURL + "/brokerage/v3/depots/%s/transactions"
+
+	// Postbox
+	PostboxURL         string = BaseURL + "/messages/clients/user/v2/documents"
+	PostboxDocumentURL string = BaseURL + "/messages/v2/documents/%s"
+
+	// Report
+	ReportURL string = BaseURL + "/reports/participants/user/v1/allbalances"
 )
